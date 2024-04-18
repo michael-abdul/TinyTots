fffb36a5b98171f6a7ac865db6eaeaf383f9a008
michael <abdulazizklichev1@gamil.com>
2024-04-18 09:36:19 +0900

    feat:connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection successfully");
+    const PORT = process.env.PORT ?? 9009;
+  })
+  .catch((err) => console.log("Error on connection MongoDB", err));
