5606d7f595a2c36956ee24856e55e7d5e53036de
michael <abdulazizklichev1@gamil.com>
2024-04-21 18:12:21 +0900

    feat:develop store process signup and login

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
-import {} from "../models/Member.service";
+import MemberService from "../models/Member.service";
+import { LoginInput, MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
 const storeController: T = {};
+const memberService = new MemberService();
+
 storeController.goHome = (req: Request, res: Response) => {
   try {
     res.send("HomePage");
@@ -26,6 +30,30 @@ storeController.getSignup = (req: Request, res: Response) => {
     console.log("getSignup");
   } catch (err) {
     console.log("Error getSignup");
+  }
+};
+
+storeController.processLogin = async (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    const input: LoginInput = req.body;
+    const result = await memberService.processLogin(input);
+    res.send(result);
+  } catch (err) {
+    console.log("Error processLogin");
+  }
+};
+
+storeController.processSignup = async (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    console.log("body", req.body);
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.STORE;
+    const result = await memberService.processSignup(newMember);
+    res.send(result);
+  } catch (err) {
+    console.log("Error processSignup");
   }
 };
 
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,8 +1,20 @@
 import express, { Router } from "express";
 const routerAdmin = Router();
 import storeController from "./controllers/store.controller";
+
+// Store
 routerAdmin.get("/", storeController.goHome);
-routerAdmin.get("/signup", storeController.getSignup);
-routerAdmin.get("/login", storeController.getLogin);
+
+routerAdmin
+  .get("/signup", storeController.getSignup)
+  .post("/signup", storeController.processSignup);
+
+routerAdmin
+  .get("/login", storeController.getLogin)
+  .post("/login", storeController.processLogin);
+
+// Product
+
+// User
 
 export default routerAdmin;
