d33acea6104b43aedca51aa50d7816f55a8ceb4c
michael <abdulazizklichev1@gamil.com>
2024-04-22 20:22:25 +0900

    feat: configure product Schema model

---
+++ src/schema/Product.model.ts
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductAge,
+  ProductCollection,
+  ProductLearn,
+  ProductStatus,
+} from "../libs/enums/product.enum";
+
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.PAUSE,
+    },
+    productCollection: {
+      type: String,
+      enum: ProductCollection,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+    productAge: {
+      type: String,
+      enum: ProductAge,
+      default: ProductAge.ONE_YEARS,
+    },
+    productLearn: {
+      type: String,
+      enum: ProductLearn,
+      default: ProductLearn.EDU_GAMES,
+    },
+    productDesc: {
+      type: String,
+      required: true,
+    },
+    productImages: {
+      type: [String],
+      default: [],
+    },
+    productViews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } //updatedAt, createdAt
+);
+productSchema.index(
+  { productName: 1, productAge: 1, productLearn: 1 },
+  { unique: true }
+);
+export default mongoose.model("Product", productSchema);
