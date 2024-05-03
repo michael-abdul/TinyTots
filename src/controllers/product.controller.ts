6236135135c25bdf435209a27f8f4a5fca090c73
michael <abdulazizklichev1@gamil.com>
2024-05-04 03:30:35 +0900

    fix: getAllProducts Api passing data into ejs

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -15,8 +15,9 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
+    const data = await productService.getAllProducts();
 
-    res.render("products");
+    res.render("products", { products: data });
   } catch (err) {
     console.log("Error getAllProducts", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -55,12 +56,9 @@ productController.updateChosenProduct = async (req: Request, res: Response) => {
     console.log("updateChosenProduct");
     const id = req.params.id;
 
+    const result = await productService.updateChosenProduct(id, req.body);
 
-    const result = await productService.updateChosenProduct(id, req.body)
-    
-    
-    res.status(HttpCode.OK).json({data:result});
-   
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     console.log("Error updateChosenProduct", err);
     if (err instanceof Errors) res.status(err.code).json(err);
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,6 +1,10 @@
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
+import {
+  Product,
+  ProductInput,
+  ProductUpdateInput,
+} from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 class ProductService {
   private readonly productModel;
@@ -8,6 +12,13 @@ class ProductService {
   constructor() {
     this.productModel = ProductModel;
   }
+
+  public async getAllProducts(): Promise<Product[]> {
+    const result = await this.productModel.find().exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
   public async createNewProduct(input: ProductInput): Promise<Product> {
     try {
       return await this.productModel.create(input);
@@ -18,16 +29,16 @@ class ProductService {
   }
 
   public async updateChosenProduct(
-    id:string,
+    id: string,
     input: ProductUpdateInput
-):Promise<Product>{
-id = shapeIntoMongooseObjectId(id);
-const result = await this.productModel.findOneAndUpdate( { _id: id}, input, {new: true})
-.exec();
-if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
-return result;
-
-}
+  ): Promise<Product> {
+    id = shapeIntoMongooseObjectId(id);
+    const result = await this.productModel
+      .findOneAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+    return result;
+  }
 }
 
 export default ProductService;
