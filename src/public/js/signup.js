0f534c0ac271bb3d087d7b814bdbab7eb3594355
michael <abdulazizklichev1@gamil.com>
2024-05-04 09:04:53 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,4 +1,62 @@
-console.log("Signup frontend javascript file");
+$(function (){
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+    fileTarget.on("change" , function (){
+        if(window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            // console.log("uploadFile",uploadFile);
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg","image/jpeg","image/png"];
+            if(!validImageType.includes(fileType)){
+                alert( "Please insert only jpeg, jpg and png!")
+
+            }else{
+                if(uploadFile){
+                
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame").attr("src", URL.createObjectURL(uploadFile)).addClass("success");
+
+                }
+                filename = $(this)[0].files[0].name;
+            }
+
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+});
+function  validateSignupForm() {  
+  const memberNick = $(".member-nick").val();
+  const memberPhone= $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if(
+      memberNick ===  "" ||
+      memberPhone === "" ||
+      memberPassword === "" ||
+      confirmPassword === ""
+      ) {
+          alert ("Please insert all required inputs!");
+          return false
+      }
+
+  if(memberPassword !== confirmPassword) {
+      alert("Password differs, please check!");
+      return false;
+  }
+
+
+  const memberImage = $(".upload-name").get(0).files[0] ? $(".upload-name").get(0).files[0].name 
+  :null;
+  if(!memberImage) {
+      alert("Please insert Store image");
+      return false;  
+  }
+ 
+}
+
+
+
 const signInBtn = document.getElementById("signIn");
 const signUpBtn = document.getElementById("signUp");
 const fistForm = document.getElementById("form1");
