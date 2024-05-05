ad9a13ac731633661eeb8a50b434fb962d1f051b
michael <abdulazizklichev1@gamil.com>
2024-05-05 15:57:48 +0900

    feat:develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,7 @@
 import {T} from "../libs/types/common";
 import  {NextFunction, Request,Response} from "express";
 import MemberService from "../models/Member.service";
-import {ExtendedRequest, LoginInput, Member, MemberInput,  } from "../libs/types/member";
+import {ExtendedRequest, LoginInput, Member, MemberInput, MemberUpdateInput,  } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { AUTH_TIMER } from "../libs/config";
 import AuthService from "../models/Auth.service";
@@ -80,6 +80,21 @@ const authService = new AuthService();
       else res.status(Errors.standard.code).json(Errors.standard);
    }
  };
+
+ memberController.updateMember = async(req: ExtendedRequest, res:Response) => {
+   try{ 
+      console.log("updateMember");
+      const input:MemberUpdateInput = req.body;
+      if(req.file) input.memberImage = req.file.path.replace(/\\/,"/");
+      const result = await memberService.updateMember(req.member, input);
+      res.status(HttpCode.OK).json(result);
+      }catch(err){
+   console.log("Error, updateMember", err)
+   if(err instanceof Errors) res.status(err.code).json(err);
+   else res.status(Errors.standard.code).json(Errors.standard);
+}
+
+ }
 
  memberController.verifyAuth = async(req:ExtendedRequest, res:Response ,next: NextFunction) => {
    try{
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,15 @@
 import express, { Router } from "express";
 const router = Router();
 import memberController from "./controllers/member.controller";
+import uploader from "./libs/utils/uploader";
 
 /** Member */
 router.post('/member/login',memberController.login);
 router.post('/member/signup',memberController.signup);
 router.post("/member/logout",memberController.verifyAuth,memberController.logout);
 router.get("/member/detail", memberController.verifyAuth,memberController.getMemberDetail);
+router.post("/member/update",memberController.verifyAuth,uploader("members").single("memberImage"),memberController.updateMember);
+
 
 /* Product*/ 
 
