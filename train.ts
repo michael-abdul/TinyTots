6ce05fde39e5b891eb404ab80e68ccd7f79852a4
michael <abdulazizklichev1@gmail.com>
2024-07-24 12:02:04 +0900

    feat: deploy project via shell & pm2

---
+++ train.ts
@@ -0,0 +1,12 @@
+// PM2 COMMANDS:
+
+import { idText } from "typescript"
+
+
+// pm2 ls 
+// pm2 start:dist/server.js --name=TINYTOTS
+// pm2 start "npm run start:prod" --name-TINYTOTS
+// pm2 stop id
+// pm2 delete id
+// pm2 restart id 
+//pm2 monit
